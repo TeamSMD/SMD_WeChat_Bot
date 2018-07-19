f90ec95dec4666e26327d354244b4c67f834c339
郭纪佑 <guojiyou40020@hotmail.com>
2018-07-20 00:26:22 +0800

    code quality imporvement

--- SMD_api.py
+++ SMD_api.py
@@ -1,12 +1,16 @@
 import requests
 
-REQUSET_URL = 'http://localhost/api'
+REQUEST_URL = 'http://localhost/api'
+
 
 class SMDapi:
     SESSION_COOKIES = {}
 
+    def __init__(self):
+        self.auth()
+
     def auth(self):
-        r = requests.get(REQUSET_URL + '/auth',
+        r = requests.get(REQUEST_URL + '/auth',
                          {'password': 'ztudMEGx0CNtTlHj1fVvkS7aJoj9QZN5BP4toSXWwsexNE1iMJXys6jTacrvbz4F'})
         if r.text == 'OK':
             print('Auth Succeed')
@@ -14,19 +18,17 @@ class SMDapi:
         else:
             print('Auth Failed')
 
-
-    def check_password(self, username: str, password: str)->bool:
+    def check_password(self, username: str, password: str) -> bool:
         try:
-            r = requests.get(REQUSET_URL + '/check_password',
-                             {'username': username, 'password': password}, cookies = self.SESSION_COOKIES)
+            r = requests.get(REQUEST_URL + '/check_password',
+                             {'username': username, 'password': password}, cookies=self.SESSION_COOKIES)
             return r.json()['ok']
         except Exception:
             return False
 
-
-    def get_coins(self, username: str)->int:
+    def get_coins(self, username: str) -> int:
         try:
-            r = requests.get(REQUSET_URL + '/get_coins', {'username': username}, cookies = self.SESSION_COOKIES)
+            r = requests.get(REQUEST_URL + '/get_coins', {'username': username}, cookies=self.SESSION_COOKIES)
             if 'error' in r.json():
                 return -1
             else:
@@ -34,17 +36,16 @@ class SMDapi:
         except Exception:
             return -2
 
-
-    def add_value(self, username: str, coins: int)->bool:
-        r = requests.get(REQUSET_URL + '/add_value', {'username': username, 'coins': coins}, cookies = self.SESSION_COOKIES)
+    def add_value(self, username: str, coins: int) -> bool:
+        r = requests.get(REQUEST_URL + '/add_value', {'username': username, 'coins': coins},
+                         cookies=self.SESSION_COOKIES)
         if r.text == 'OK':
             return True
         else:
             return False
 
-
-    def check_user_exists(self, username: str)->bool:
-        r = requests.get(REQUSET_URL + '/check_user_exists', {'username': username}, cookies = self.SESSION_COOKIES)
+    def check_user_exists(self, username: str) -> bool:
+        r = requests.get(REQUEST_URL + '/check_user_exists', {'username': username}, cookies=self.SESSION_COOKIES)
         if r.text == 'True':
             return True
         else:
